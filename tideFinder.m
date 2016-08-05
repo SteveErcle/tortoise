8592b1f9125d08ff33ce4c32ccbed465046d67c0
SteveErle <therevolution777@yahoo.com>
2016-08-04 21:45:44 -0400

    testing with FFT

--- tideFinder.m
+++ tideFinder.m
@@ -3,7 +3,7 @@ clc; close all; clear all;
 td = TurtleData;
 ta = TurtleAuto;
 
-stockNum = [11];
+stockNum = [31];
 lenOfData = '10d';
 durationOfCandle = '600';
 
@@ -35,95 +35,95 @@ title(strcat(stock, ' Candles'))
 
 to = TurtleOptimizer;
 
-% x = [0.25 0.5, 40, 110, 0, pi]
+x = [0.25 0.5, 12, 30, 0, pi];
 
-% t = range(12:300);
-% 
-% opt = @(x)(sum(abs(ma.STOCK(12:300) - to.genWave(x(1:2),x(3:4),x(5:6),t, mean(ta.cl.STOCK)))));
-% options = optimoptions('particleswarm','SwarmSize',200,'HybridFcn',@fmincon);
-% 
-% 
-% nvars = 6;
-% lb = [0, 0, 37,  90, -pi, -pi];
-% ub = [2, 5, 53, 130,  pi,  pi];
-% 
-% x = particleswarm(opt,nvars,lb,ub, options)
-% 
-% t = 12:1999;
-% y = to.genWave(x(1:2),x(3:4),x(5:6),t, mean(ta.cl.STOCK));
-% plot(t,y,'k')
+t = range(12:300);
+
+opt = @(x)(sum(abs(ma.STOCK(12:300) - to.genWave(x(1:2),x(3:4),x(5:6),t, mean(ta.cl.STOCK)))));
+options = optimoptions('particleswarm','SwarmSize',500,'HybridFcn',@fmincon);
 
 
-% By  = [nan; diff(y)];
-% Bma = [nan; diff(ma.STOCK(12:1999))];
-% ByBin  = By  > 0;
-% BmaBin = Bma > 0;
-% Bside = ByBin == BmaBin;
-% BsideUpper = ByBin(find(Bside) > 0);
-% upper = find(ByBin(find(Bside)) > 0);
+nvars = 6;
+lb = [0, 0, 7,  20, -pi, -pi];
+ub = [2, 5, 20, 40,  pi,  pi];
 
-% setterUpper = [];
-% x = BsideUpper;
-% z = find(x == 1);
-% setterUpper = [];
-% for i = 1:length(z)-1
-%     setterUpper = [setterUpper; z(i), z(i+1)-1+1];
-% end
+x = particleswarm(opt,nvars,lb,ub, options);
 
+t = 12:length(ta.cl.STOCK);
+y = to.genWave(x(1:2),x(3:4),x(5:6),t, mean(ta.cl.STOCK));
+plot(t,y,'k')
 
-% t = 12:300;
-% y = to.genWave(x(1:2),x(3:4),x(5:6),t, mean(ta.cl.STOCK));
-% plot(t,y,'c')
-
-
-%
-% BmaPos = [nan; diff(Bma > 0)];
-% oner    = find(BmaPos == 1);
-% negOner = find(BmaPos == -1);
-% allPos = [oner, negOner+1];
-%
-% BmaNeg = [nan; diff(Bma <= 0)];
-% oner    = find(BmaNeg == 1);
-% negOner = find(BmaNeg == -1);
-% oner(end) = [];
-% allNeg = [oner, negOner+1];
 
 Bma = [nan; diff(ma.STOCK(12:end))];
 Bma = [nan(11,1); Bma];
 
+By  = [nan; diff(y)];
+By = [nan(11,1); By];
+
+t = 12:300;
+y = to.genWave(x(1:2),x(3:4),x(5:6),t, mean(ta.cl.STOCK));
+plot(t,y,'c')
+
+
+
 inMarket.BULL = [];
-enter = 0;
-for i = 50:length(ta.cl.STOCK)
+inMarket.BEAR = [];
+enter.BULL = 0;
+enter.BEAR = 0;
+examine.BULL = 1;
+examine.BEAR = 1;
+
+thres = 0.5;
+
+for i = 50:length(ta.cl.STOCK)-1
     
-    if Bma(i) > 0
-        
-        if enter == 0
-            inMarket.BULL = [inMarket.BULL; i, nan];
+    if examine.BULL == 1
+        if Bma(i) > max(Bma)*0 && By(i) > max(By)*thres
+            if enter.BULL == 0
+                inMarket.BULL = [inMarket.BULL; i, nan];
+            end
+            enter.BULL = 1;
+        else
+            if enter.BULL == 1
+                inMarket.BULL(end,2) = i;
+            end
+            enter.BULL = 0;
         end
-        
-        enter = 1;
-        
-    else
-        
-        if enter == 1
-            inMarket.BULL(end,2) = i;
+    end 
+    
+    if examine.BEAR == 1
+        if Bma(i) < max(Bma)*0 && By(i) < min(By)*thres
+            if enter.BEAR == 0
+                inMarket.BEAR = [inMarket.BEAR; i, nan];
+            end
+            enter.BEAR = 1;
+        else
+            if enter.BEAR == 1
+                inMarket.BEAR(end,2) = i;
+            end
+            enter.BEAR = 0;
         end
-        
-        enter = 0;
-        
-    end
+    end 
+    
+    
 end
 
-
-
+if ~isempty(inMarket.BULL), inMarket.BULL(end,:) = []; end 
+if ~isempty(inMarket.BEAR), inMarket.BEAR(end,:) = []; end 
 
 tz = TurtleAnalyzer;
 
-roiPos = tz.percentDifference(ta.cl.STOCK(inMarket.BULL(:,1)), ta.cl.STOCK(inMarket.BULL(:,2)));
-sum(roiPos)
-% 
-% roiNeg = -tz.percentDifference(ta.cl.STOCK(allNeg(:,1)), ta.cl.STOCK(allNeg(:,2)));
-% sum(roiNeg)
+if examine.BULL == 1
+    roiPos = tz.percentDifference(ta.cl.STOCK(inMarket.BULL(:,1)), ta.cl.STOCK(inMarket.BULL(:,2)));
+    disp([sum(roiPos), length(roiPos)]);
+end
+
+if examine.BEAR == 1
+    roiNeg = -tz.percentDifference(ta.cl.STOCK(inMarket.BEAR(:,1)), ta.cl.STOCK(inMarket.BEAR(:,2)));
+    disp([sum(roiNeg), length(roiNeg)]);
+end
+
+
 
 
 % delete(slider);
@@ -155,19 +155,19 @@ for i = 1:length(inMarket.BULL)
     
 end
 
-% for i = 1:length(allNeg)
-%     
-%     xLo = allNeg(i,1);
-%     xHi = allNeg(i,2);
-%     
-%     x = [xLo xHi xHi xLo];
-%     y = [yLo yLo yHi yHi];
-%     
-%     color = [1, .7, .7];
-%     
-%     hp = patch(x,y, color, 'FaceAlpha', 0.25);
-%     
-% end
+for i = 1:length(inMarket.BEAR)
+    
+    xLo = inMarket.BEAR(i,1);
+    xHi = inMarket.BEAR(i,2);
+    
+    x = [xLo xHi xHi xLo];
+    y = [yLo yLo yHi yHi];
+    
+    color = [1, .7, .7];
+    
+    hp = patch(x,y, color, 'FaceAlpha', 0.25);
+    
+end
 % 
 % while(true)
 %     
